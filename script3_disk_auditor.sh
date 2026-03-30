7b7a220288f7cc197c55c1ab22e30f5ce8638ca6
parikshit1218 <parikshitc76@gmail.com>
2026-03-30 21:23:13 +0530

    script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script3_disk_auditor.sh
-# Author: Aditya Gupta
-# Roll Number: 24BCE10151
+# Author: Parikshit chaudhary
+# Roll Number: 24BAI10188
 # Purpose: Audit important directories for size and permissions, and check Git config
 
 echo "==========================================="
